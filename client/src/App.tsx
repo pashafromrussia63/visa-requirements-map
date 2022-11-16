8ea92f1883531d443cb54f19843530094073ca2c
Cyberpasha <pashafromrussia63@gmail.com>
2022-11-16 20:21:31 +0500

    Add flags to sidebar, add zoom controls

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { CircleFlag } from 'react-circle-flags';
 import ReactTooltip from "react-tooltip";
 import './App.scss';
 import MapChart from './components/Map/Map';
 
 function App() {
   const [content, setContent] = useState("");
-  const [sidebar, setSidebar] = useState('');
+  const [sidebar, setSidebar] = useState({
+    name: '',
+    iso2: '',
+    status: '',
+    stay: 0
+  });
+  const [sidebarVisiblity, setSidebarVisibility] = useState(false);
+
+  useEffect(() => {
+    console.log('sidebar', sidebar);
+  }, [sidebar]);
 
   return (
     <div className="app">
       <div className="map">
         <div data-tip="">
-          <MapChart setTooltipContent={setContent} setSidebarContent={setSidebar}/>
+          <MapChart setTooltipContent={setContent} setSidebarContent={setSidebar} setSidebarVisibility={setSidebarVisibility}/>
         </div>
         <ReactTooltip>{content}</ReactTooltip>
       </div>
-      <button className='button' onClick={() => setSidebar('')}>Reset</button>
       <div
-        className={`sidebar ${!!sidebar ? 'sidebar--isOpen' : ''}`}
-      >{sidebar}</div>
+        className={`sidebar ${sidebarVisiblity ? 'sidebar--isOpen' : ''}`}
+      >
+        <button className='button' onClick={() => setSidebarVisibility(false)}>X</button>
+        <div className="sidebar-flag">
+          {
+            sidebar.iso2 ?  <CircleFlag countryCode={sidebar.iso2} height="200" /> : null
+          }
+        </div>
+        <div>{sidebar.name}</div>
+        <div>{sidebar.status}</div>
+        {
+          (sidebar.stay) ? <div>Duration of stay: {sidebar.stay}</div> : null
+        }
+      </div>
     </div>
   );
 }
--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { csv } from "d3-fetch";
+import './Map.scss';
 
 import {
   ComposableMap,
   Geographies,
-  Geography
+  Geography,
+  ZoomableGroup
 } from "react-simple-maps";
 
 const geoUrl = "/world.json";
 
 const getColor = (d) => {
   if(!d) return '#ababab';
-  switch(d['Status']) {
+  switch(d.status) {
     case 'Visa not required': return '#22b14c';
     case 'eVisa / Visa on arrival': return '#79d343';
     case 'eVisa': return '#61c29c';
@@ -24,8 +26,26 @@ const getColor = (d) => {
   }
 };
 
-const MapChart = ({setTooltipContent, setSidebarContent}) => {
+const MapChart = ({setTooltipContent, setSidebarContent, setSidebarVisibility}) => {
   const [data, setData] = useState([]);
+  const [position, setPosition] = useState({ coordinates: [0, 0], zoom: 1 });
+
+  const mapWidth = 800;
+  const mapHeight = 600;
+
+  function handleZoomIn() {
+    if (position.zoom >= 4) return;
+    setPosition((pos) => ({ ...pos, zoom: pos.zoom * 2 }));
+  }
+
+  function handleZoomOut() {
+    if (position.zoom <= 1) return;
+    setPosition((pos) => ({ ...pos, zoom: pos.zoom / 2 }));
+  }
+
+  function handleMoveEnd(position) {
+    setPosition(position);
+  }
 
   useEffect(() => {
     // get values for data
@@ -35,57 +55,94 @@ const MapChart = ({setTooltipContent, setSidebarContent}) => {
   }, []);
 
   return (
-    <ComposableMap
-      projection = "geoMercator"
-      projectionConfig = {{
-        rotate: [-10, 0, 0],
-        center: [0, -20],
-        scale: 90
-      }}
-    >
-      {/* <Sphere stroke="#E4E5E6" strokeWidth={0.5} /> */}
-      {data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              console.log(geo.id);
-              const d = data.find((s) => s.ISO3 === geo.id);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  backgroundColor="skyblue"
-                  fill={getColor(d)}
-                  style={{
-                    default: {
-                      outline: "none"
-                    },
-                    hover: {
-                      opacity: 0.8,
-                      cursor: 'pointer',
-                      outline: "none"
-                    },
-                    pressed: {
-                      opacity: 0.5,
-                      outline: "none"
-                    }
-                  }}
-                  onMouseEnter={() => {
-                    setTooltipContent(`${geo.properties.name}`);
-                  }}
-                  onMouseLeave={() => {
-                    setTooltipContent("");
-                  }}
-                  onClick={() => {
-                    setSidebarContent(`${geo.properties.name}`);
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
-    </ComposableMap>
+    <div className="map">
+      <ComposableMap
+        projection = "geoMercator"
+        projectionConfig = {{
+          rotate: [-10, 0, 0],
+          center: [0, -20],
+          scale: 90
+        }}
+      >
+        {data.length > 0 && (
+          <ZoomableGroup
+            zoom={position.zoom}
+            center={position.coordinates}
+            onMoveEnd={handleMoveEnd}
+            translateExtent={[
+              [-200, -200],
+              [mapWidth + 200, mapHeight + 200]
+            ]}
+          >
+            <Geographies geography={geoUrl}>
+              {({ geographies }) =>
+                geographies.map((geo) => {
+                  const d = data.find((s) => s.iso3 === geo.id);
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      fill={getColor(d)}
+                      style={{
+                        default: {
+                          outline: "none"
+                        },
+                        hover: {
+                          opacity: 0.8,
+                          cursor: 'pointer',
+                          outline: "none"
+                        },
+                        pressed: {
+                          opacity: 0.5,
+                          outline: "none"
+                        }
+                      }}
+                      onMouseEnter={() => {
+                        setTooltipContent(`${geo.properties.name}`);
+                      }}
+                      onMouseLeave={() => {
+                        setTooltipContent("");
+                      }}
+                      onClick={() => {
+                        setSidebarVisibility(true);
+                        setSidebarContent(d);
+                      }}
+                    />
+                  );
+                })
+              }
+            </Geographies>
+          </ZoomableGroup>
+        )}
+      </ComposableMap>
+      <div className="controls">
+          <button onClick={handleZoomIn}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="3"
+            >
+              <line x1="12" y1="5" x2="12" y2="19" />
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </button>
+          <button onClick={handleZoomOut}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth="3"
+            >
+              <line x1="5" y1="12" x2="19" y2="12" />
+            </svg>
+          </button>
+        </div>
+      </div>
   );
 };
 
